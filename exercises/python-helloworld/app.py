425b737a93df271c82e3a8ecf0fe8f37c2f802ec
Bernardo Garcia <bernardo.garcia@energyworx.org>
2022-10-08 17:14:44 +0200

    Generate JSON response for metrics and status endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask import jsonify
+# from flask_api import status
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/status')
+def health_check():
+    health_check = {'result': 'OK - Healthy'}
+    profile = {'user' : 'admin'}
+    return jsonify(profile,health_check)
+    # return health_check, status.HTTP_200_OK
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+
+    response = [
+        {'user' : 'admin'},
+        'data: { UserCount: 140, UserCountActive: 23}'
+    ]
+    return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
